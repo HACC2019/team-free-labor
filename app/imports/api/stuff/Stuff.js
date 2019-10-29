59277d11c6ce9835d1da405cac414bb6f0c4eea8
Arslan Rakhmankulov <31532955+arslan-r@users.noreply.github.com>
2019-10-29 01:04:44 -1000

    Started on form
    need to find out how to activate "other" button if a certain selection ahs been made.
    OR.
    How to incorporate an optional field into a schema.
    Next step: Make this small piece of form work 100%. Fill out the rest of the form later

--- app/imports/api/stuff/Stuff.js
+++ app/imports/api/stuff/Stuff.js
@@ -1,20 +1,51 @@
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
-
-/** Define a Mongo collection to hold the data. */
+/** RENAME FILE TO SOMETHING ELSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE */
+/** Define a Mongo collection to hold the data.
+ * ['', '', '', '', '', '', '', '', '', '', ], */
 const Stuffs = new Mongo.Collection('Stuffs');
+
+/** How to implement "other". IF other is checked, make a form available" */
+const AppFormValues = {
+  howDidYouHearAbout: ['Contractor', 'Community Organization', 'News/Radio/TV', 'Online (Internet)', 'Utility',
+  'Public Event', 'Hawaii Energy', 'Friends & Family'],
+  whichDoYouHaveInHome: ['Washer', 'Dryer', ' Kitchen Refrigerator', '2nd Refrigerator',
+    'Chest Freezer', 'Solar Hot Water heater', 'Solar PV System', 'LED or CFL Light Bulbs', 'WIFI'],
+  interestedInInstalling: ['Washer', 'Dryer', 'Kitchen Refrigerator', 'Solar Hot Water Heater',
+    'Solar PV System', 'LED or CFL Light Bulbs'],
+  assistanceFrom: ['Church', 'Community Event', 'School/Class', 'Online', 'Accountant/Tax Preparer',
+    'Financial Advisor'],
+  wouldLikeToInstall: ['Solar Thermal Hot Water Heater', 'Solar PV Water Heater', 'Heat Pump Water Heater', 'Solar PV System'],
+  whichIsland: ['Oahu', 'Maui', 'Lanai', 'Molokai', 'Hawaii'],
+
+
+};
+
 
 /** Define a schema to specify the structure of each document in the collection. */
 const StuffSchema = new SimpleSchema({
-  name: String,
-  quantity: Number,
-  owner: String,
-  condition: {
-    type: String,
-    allowedValues: ['excellent', 'good', 'fair', 'poor'],
-    defaultValue: 'good',
-  },
+  howDidYouHearAbout: { type: String, optional: true, allowedValues: AppFormValues.howDidYouHearAbout },
+  otherHDYHA: String,
+  whichDoYouHaveInHome: { type: String, optional: true, allowedValues: AppFormValues.whichDoYouHaveInHome },
+  interestedInInstalling: { type: String, optional: true, allowedValues: AppFormValues.interestedInInstalling },
+  assistanceFrom: { type: String, optional: true, allowedValues: AppFormValues.assistanceFrom },
+  anyoneYouKnowName: String,
+  anyoneYouKnowPhone: Number,
+  anyoneYouKnowEmail: String,
+  nameOnUtilAcc: String,
+  utilAccNum: Number,
+  wouldLikeToInstall: { type: String, optional: true, allowedValues: AppFormValues.wouldLikeToInstall },
+  approvedContractorName: String,
+  approvedContractorContact: String,
+  installAddress: String,
+  whichIsland: { type: String, allowedValues: AppFormValues.assistanceFrom },
+
+
+
+
+
+
 }, { tracker: Tracker });
 
 /** Attach this schema to the collection. */
